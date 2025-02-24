127c07205836b6581490554992887a6030a64d5f
Elijah Lee <eij285@gmail.com>
2025-02-25 08:05:54 +1100

    stub functions for score.py

---
+++ score.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+# Scores button tile through breadth search
+def scoreButton(graph):
+    pass
+
+# Scores cat tile through breadth search
+def scoreCat(graph):
+    pass
+
+# Scores design tile by checking neighbouring nodes and seeing if its satisfied
+def scoreDesignTile(graph):
+    pass
