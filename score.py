32715d85e3ea4769004c12a2232ac617d90340b6
Elijah Lee <eij285@gmail.com>
2025-03-04 12:34:05 +1100

    modifications to params for placeholder functions and boilerplate code with missing logic in comments for scoreButton

--- score.py
+++ score.py
@@ -1,13 +1,33 @@
 import networkx as nx
+from Errors import InvalidTokenError
 
-# Scores button tile through breadth search
-def scoreButton(graph):
-    pass
+
+# Scores button tile by verifying valid placement through breadth search
+def scoreButton(g, node, button_color):
+    # Check neighouring tiles until we find 3 that have the same color
+    queue = g.neighbors(node)
+    
+    # Write a visited 
+
+    valid_nodes = 0
+
+    for elem in queue:
+        if button_color == elem['color']:
+            valid_nodes += 1
+
+            # Need to stop duplicate elements
+            queue.append(elem.neighbors(node))
+        
+    
 
 # Scores cat tile through breadth search
-def scoreCat(graph):
-    pass
+def scoreCat(graph, value, type, design):
+    return value
 
 # Scores design tile by checking neighbouring nodes and seeing if its satisfied
-def scoreDesignTile(graph):
-    pass
+def scoreDesignTile(graph, value1, value2):
+    return 0
+
+
+# if __name__ == '__main__':
+#     graph = nx.Graph()
